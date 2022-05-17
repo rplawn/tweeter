6f9a953ca0f633440c42ca1534a169231d46ec28
roop <rplawn@icloud.com>
2022-05-16 17:08:29 -0700

    adding character counter for tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
   // --- our code goes here ---
   // console.log("This is working")
+  const counter = $(".counter")
+  $("#tweet-text").on("input", (event)=>{
+    const value = event.target.value
+    const length = value.length
+    const remaining = 140 - length
+    counter.text(remaining)
+    if (remaining < 0) {
+      counter.addClass("counter-warning")
+    } else {
+      counter.removeClass("counter-warning")
+    }
+  })
 });
+
+//Shorthand line 6-9:
+// counter.text(140 - event.target.value.length)
+// line 10-14
+// counter.toggleClass("counter-warning", remaining < 0)
+// debugger to debug
