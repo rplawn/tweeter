9b6cacf0e638fe30138a1a8ef2b315ed40368d71
roop <rplawn@icloud.com>
2022-05-23 20:44:03 -0700

    added validation to form and refetch tweets automatically on submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,59 +5,50 @@
  */
 
 
-$(document).ready(function(){
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+$(document).ready(function () {
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  for (let tweet of tweets) {
-  // calls createTweetElement for each tweet
-   const tweetElement = createTweetElement(tweet)
-   // takes return value and appends it to the tweets container
-   $('.tweets').prepend(tweetElement)
-}
-}
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
-const createTweetElement = function(tweet) {
-  let $tweet = $("<article>").addClass("tweet")
-let html = `
-<article class="postedTweets">
+
+
+  const createTweetElement = function (tweet) {
+    let tweetElement = `
+<article class="tweet">
         <header class="tweetHeader">
-          <div>
-            ${tweet.user.avatars}
+          <div> 
+             <img src="${tweet.user.avatars}" alt="User Avatar" />
+             <p>${tweet.user.name}</p>
           </div>
-            ${tweet.user.handle}
+          <p> ${tweet.user.handle} </p>
         </header>
-        <div class="tweetContent">
-          ${tweet.content.text}
-        </div>
+        <div class="tweetContent"> ${tweet.content.text} </div>
         <hr>
         <footer class="tweetFooter">
-            ${tweet.created_at}
+            <span> ${timeago.format(tweet.created_at)} </span>
             <div>
               <i class="fa-solid fa-flag"></i>
               <i class="fa-solid fa-retweet"></i>
@@ -66,33 +57,72 @@ let html = `
         </footer>
       </article>
 `
-let tweetElement = $tweet.append(html)
-return tweetElement;
-}
+    return tweetElement;
+  }
 
-renderTweets(data);
-
-// event listener for submit in tweet form-->
- $( "#tweetForm" ).on( "submit", function(event) {
-   event.preventDefault();
-
-   const serializedContent =  $(this).serialize();
-  //  console.log("this is what i need" , serializedContent);
-  // AJAX post request for submission data
-
-  $.ajax({
-    type: "POST",
-    url: "http://localhost:8080/tweets/",
-    data: serializedContent,
-    success: function(data) {
-      console.log("this is a success", data)
-    }, 
-    error: function(err) {
-      console.log("Error!", err)
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    for (let tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const tweetElement = createTweetElement(tweet)
+      // takes return value and appends it to the tweets container
+      $('.tweets').prepend(tweetElement)
     }
-  });
+  }
+
+   // AJAX GET request
+   const loadTweets = function () {
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:8080/tweets/",
+      dataType: "json",
+      success: function (data) {
+        console.log("tweets loaded successfully", data);
+        renderTweets(data);
+      },
+      error: function (err) {
+        console.log("Error!", err);
+      }
+    });
+  }
+
+  // event listener for submit in tweet form-->
+  $("#tweetForm").on("submit", function (event) {
+    event.preventDefault();
+
+    // define variables
+    const textLength = $("#tweet-text").val().length
+    const serializedContent = $(this).serialize();
+
+    //if checks
+    if (textLength === 0) {
+      return alert("Please enter some text");
+    }
+
+    if (textLength > 140) {
+      return alert("Tweet must be less than 140 characters");
+    }
+
+    // AJAX post request for submission data
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/tweets/",
+      data: serializedContent,
+      success: function (data) {
+        console.log("Post request a success", data);
+        //refetching tweets on form submission
+        loadTweets();
+      },
+      error: function (err) {
+        console.log("Error!", err);
+      }
+    });
 
   });
+
+ 
+//Loading tweets on page
+  loadTweets();
 
 })
 
